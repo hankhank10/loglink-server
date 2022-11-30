0a8d09ed3f3f69be8a8f77584327225b502f9e1a
hankhank10 <m.a.hankinson@gmail.com>
2022-11-30 21:50:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 from datetime import datetime, time, timedelta
 import secrets
+import logging
 from dataclasses import dataclass
 
 # Import flask
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -16,112 +17,75 @@ import secretstuff
 app = Flask(__name__)
 
 # Create the DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whatsapp.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JSON_SORT_KEYS"] = False
 app.config['SECRET_KEY'] = secretstuff.app_secret_key
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Set up Heyoo
+from heyoo import WhatsApp
+messenger = WhatsApp(
+    token=secretstuff.whatsapp_token,
+    phone_number_id=secretstuff.whatsapp_phone_number_id
+)
 
-# Define the model in which the user data and tokens are stored
+
+# Define the model in which the user data, tokens and messages are stored
 @dataclass
-class SpotifyUser(db.Model):
-    spotify_token_is_current:bool
+class User(db.Model):
+    id:int = db.Column(db.Integer, primary_key=True)
 
-    id:str = db.Column(db.String(80), primary_key=True)
-    spotify_username:str = db.Column(db.String(80), unique=True)
-    spotify_token:str = db.Column(db.String(120), unique=True)
-    spotify_token_expires_at:datetime = db.Column(db.DateTime)
-    spotify_refresh_token:str = db.Column(db.String(120), unique=True)
-    spotify_token_last_refreshed:datetime = db.Column(db.DateTime)
-    last_called:datetime = db.Column(db.DateTime)
-    api_calls:int = db.Column(db.Integer)
+    token = db.Column(db.String(80), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+
+    api_call_count:int = db.Column(db.Integer, default=0)
+
+
+@dataclass
+class Message(db.Model):
+    id:int = db.Column(db.Integer, primary_key=True)
+
+    whatsapp_message_id:str = db.Column(db.String(100), nullable=False)
+
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+
+    contents:str = db.Column(db.String(5000))
+    timestamp:datetime = db.Column(db.DateTime)
+    delivered:bool = db.Column(db.Boolean, default=False)
 
     @property
-    def spotify_token_is_current(self):
-        return self.spotify_token_expires_at > datetime.now()
+    def minutes_old(self):
+        return (datetime.now() - self.timestamp).seconds / 60
+
 
 
 # Define global paths and uris
-app_uri = "https://spotify.logspot.top/"
-redirect_path = "callback/"
-redirect_uri = app_uri + redirect_path
+app_uri = "https://whatsapp.logspot.top/"
 
-spotify_api_recently_listened_uri = "https://api.spotify.com/v1/me/player/recently-played"
-spotify_api_user_uri = "https://api.spotify.com/v1/me"
-spotify_api_token_uri = "https://accounts.spotify.com/api/token"
+
+
+#################
+# HOUSEKEEPING #
+#################
+
+
+def delete_delivered_messages(user_id):
+    messages = Message.query.filter_by(user_id=user_id, delivered=True)
+    for message in messages:
+        db.session.delete(message)
+    db.session.commit()
+
+
 
 
 #####################
-# SPOTIFY API CALLS #
+# WHATSAPP API CALLS #
 #####################
 
 
-def get_user_id(token = None):
-    if token is None:
-        return None
 
-    headers = {"Authorization": "Bearer " + token}
-
-    response = requests.get(spotify_api_user_uri, headers=headers)
-    return response.json()['id']
-
-
-def get_recently_listened(token):
-    midnight = datetime.combine(datetime.today(), time.min)
-    midnight = int(midnight.timestamp()) * 1000
-
-    # Request the data from Spotify
-    headers = {"Authorization": "Bearer " + token}
-    params = {
-        "limit": 50,
-        "after": midnight
-    }
-    response = requests.get(spotify_api_recently_listened_uri, headers=headers, params=params)
-
-    # Parse the response
-    json_response = response.json()
-    track_names = []
-
-    for item in json_response['items']:
-        track_name = item['track']['name']
-        artist_name = item['track']['artists'][0]['name']
-
-        track_names.append({
-            'artist': artist_name,
-            'track_name': track_name
-        })
-
-    track_names.reverse()
-
-    return {
-        'children': track_names
-    }
-
-
-def get_new_spotify_token(spotify_code=None, refresh_token=None):
-
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    auth = HTTPBasicAuth(secretstuff.spotify_client_id, secretstuff.spotify_client_secret)
-
-    if spotify_code:
-        params = {
-            "grant_type": "authorization_code",
-            "code": spotify_code,
-            "redirect_uri": redirect_uri,
-        }
-    elif refresh_token:
-        params = {
-            "grant_type": "refresh_token",
-            "refresh_token": refresh_token,
-        }
-    else:
-        # This is an error as a spotify_code or refresh_token is required
-        return None
-
-    response = requests.post(spotify_api_token_uri, params=params, headers=headers, auth=auth)
-    return response
 
 
 #####################
@@ -131,151 +95,114 @@ def get_new_spotify_token(spotify_code=None, refresh_token=None):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello world"
 
 
-# This redirects the user to the spotify authentication API. It is here in a route rather than in the HTML because
-# it involves passing a client_id which I would rather not expose openly
-@app.route('/authenticate')
-def spotify_authenticate_redirect():
-    auth_uri = "https://accounts.spotify.com/authorize" + \
-               "?client_id=" + secretstuff.spotify_client_id + \
-               "&response_type=code" + \
-               "&redirect_uri=" + redirect_uri + \
-               "&scope=user-read-recently-played"
-    return redirect(auth_uri)
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == "GET":
+        if request.args.get("hub.verify_token") == secretstuff.verify_token:
+            logging.info("Verified webhook")
+            response = make_response(request.args.get("hub.challenge"), 200)
+            response.mimetype = "text/plain"
+            return response
+        logging.error("Webhook Verification failed")
+        return "Invalid verification token"
+
+    data = request.get_json()
+    # logging.info("Received webhook data: %s", data)
+
+    changed_field = messenger.changed_field(data)
+    if changed_field == "messages":
+        new_message = messenger.get_mobile(data)
+        if new_message:
+
+            # Get all the details of the sender
+            mobile = messenger.get_mobile(data)
+            name = messenger.get_name(data)
+            message_type = messenger.get_message_type(data)
+
+            logging.info(
+                f"New Message; sender:{mobile} name:{name} type:{message_type}"
+            )
+            if message_type == "text":
+                # Check if this is a new user
+                user = User.query.filter_by(phone_number=mobile).first()
+
+                # If it is a new user, create the account and return the token
+                if not user:
+                    user = User(phone_number=mobile, token="whatsapp"+secrets.token_hex(4))
+                    db.session.add(user)
+                    db.session.commit()
+                    logging.info("New user: %s", mobile)
+                    messenger.send_message("Welcome to LogText, your token will be send in the next message", mobile)
+                    messenger.send_message(user.token, mobile)
+                    return "ok"
+
+                # If it is an existing user, add the message to the database
+                if user:
+
+                    message_id = messenger.get_message_id(data)
+
+                    user.api_call_count = user.api_call_count + 1
+                    db.session.commit()
+
+                    #logging.info("User: %s, API calls: %s", mobile, user.api_call_count)
+
+                    new_message = Message(
+                        user_id=user.id,
+                        whatsapp_message_id=message_id,
+                        contents=messenger.get_message(data),
+                        timestamp=datetime.now(),
+                    )
+                    db.session.add(new_message)
+                    db.session.commit()
+                    #logging.info("New message: %s", new_message.contents)
+
+                    #messenger.send_message(new_message.contents, mobile)
+
+                    return "ok"
+
+    return "ok"
 
 
-# This is the workflow the first time the user authenticates. Spotify sends its code back to this callback route after
-# the user authenticates successfully
-@app.route('/callback/')
-def auth_callback():
-    # Get the 'code' from the callback sent by Spotify
-    spotify_code = request.args.get('code')
+@app.route('/get_new_messages/<token>/', methods=['GET'])
+def get_new_messages(token):
+    user = User.query.filter_by(token=token).first()
 
-    if not spotify_code:
-        flash ("Login error: no code found in the information returned by Spotify")
-        return redirect(url_for('index'))
-
-    # Use the code returned by Spotify to request a token from Spotify
-    response = get_new_spotify_token(spotify_code)
-
-    print (response.json())  # for debug
-
-    if not response.ok:
-        flash ("Login error: error getting token from spotify")
-        return redirect(url_for('index'))
-
-    # Get the access token returned by Spotify
-    access_token = response.json()['access_token']
-
-    # Use the token to get the user's Spotify username
-    spotify_username = get_user_id(access_token)
-
-    # Check whether the Spotify username is already in the database
-    user = SpotifyUser.query.filter_by(
-        spotify_username = spotify_username
-    ).first()
-
-    # If that spotify username is not in the database already then create a new user record ...
     if not user:
-        new_id = "spotify"+secrets.token_hex(15)
+        return jsonify({"error": "Invalid token"}), 401
 
-        user = SpotifyUser(
-            id = new_id,
-            spotify_username = spotify_username,
-            spotify_token = access_token,
-            spotify_token_expires_at = datetime.now() + timedelta(seconds=response.json()['expires_in']),
-            spotify_refresh_token = response.json()['refresh_token'],
-            api_calls = 0,
-            spotify_token_last_refreshed = datetime.now()
-        )
-        db.session.add(user)
-    # ... or update the user's token if the user is in the database
-    else:
-        user.spotify_token = access_token
-        user.spotify_token_expires_at = datetime.now() + timedelta(seconds=response.json()['expires_in'])
-        user.spotify_token_last_refreshed = datetime.now()
+    messages = Message.query.filter_by(user_id=user.id).all()
+
+    new_messages = []
+    for message in messages:
+        if not message.delivered:
+            message.delivered = True
+            new_messages.append(message)
 
     db.session.commit()
 
-    return render_template(
-        'success.html',
-        id=user.id
-    )
+    if new_messages:
+        if len(new_messages) == 1:
+            response = "1 new message downloaded by LogSeq"
+        else:
+            response = f"{len(new_messages)} new messages downloaded by LogSeq"
+        messenger.send_message(response, user.phone_number)
 
-
-@app.post('/getsongs/')
-def get_songs():
-
-    # Check that we have been sent JSON and that it is valid and matches a user
-    try:
-        posted_json = request.get_json()
-        user_id = posted_json['user_id']
-    except:
-        return jsonify({
-            'status': 'error',
-            'error_type': 'failure_parsing_json',
-            'message': 'Failure parsing JSON or no JSON received'
-        }), 400
-
-    print (posted_json)
-
-    if not user_id:
-        return {
-            'status': 'error',
-            'error_type': 'no_user_id',
-            'message': 'No user_id provided in JSON'
-        }, 400
-
-    user = SpotifyUser.query.filter_by(id = user_id).first()
-
-    if not user:
-        return {
-            'status': 'error',
-            'error_type': 'user_not_found',
-            'message': 'No user found with that token. Try refreshing your token at ' + app_uri + ' and is ensure it is correctly entered in LogSeq settings.'
-        }, 404
-
-    # Check if the access token is current, and if not request a refreshed one
-    if not user.spotify_token_is_current:
-        # Request the token from Spotify
-        response = get_new_spotify_token(refresh_token=user.spotify_refresh_token)
-
-        # Check it is good
-        if not response.ok:
-            return {
-                'status': 'error',
-                'error_type': 'error_refreshing_token',
-                'message': 'Error fetching refreshed authentication from Spotify. Try refreshing your token at ' + app_uri + ' and is ensure it is correctly entered in LogSeq settings.'
-            }, 500
-
-        # Update the database record
-        user.spotify_token = response.json()['access_token']
-        user.spotify_token_expires_at = datetime.now() + timedelta(seconds=response.json()['expires_in'])
-        user.spotify_token_last_refreshed = datetime.now()
-        user.api_calls = 0
-        db.session.commit()
-
-    # Get the tracks from spotify
-    try:
-        recently_listened_tracks = get_recently_listened(user.spotify_token)
-    except:
-        return {
-            'status': 'error',
-            'error_type': 'song_list_failed',
-            'message': 'Spotify did not return a valid song list.'
-        }, 400
+    delete_delivered_messages(user.id)
 
     return jsonify({
+        'status': 'success',
         'user': {
-            'id': user.id,
-            'spotify_token_expires_at': user.spotify_token_expires_at.isoformat(),
-            'spotify_token_is_current': user.spotify_token_is_current,
-            'server_time': datetime.now().isoformat(),
+            'token': user.token,
+            'api_call_count': user.api_call_count,
         },
-        'data': recently_listened_tracks
-    })
+        'messages': {
+            'count': len(new_messages),
+            'contents': new_messages
+        }
+    }), 200
 
 
 # Run the app
