bf0ed23a9adb371218b90d643d469da555dccef2
hankhank10 <m.a.hankinson@gmail.com>
2022-12-27 11:04:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def encrypt_at_rest(string):
         return string
 
 def decrypt_at_rest(string):
-    return str(fernet.decrypt(string))
+    return str(fernet.decrypt(string.decode()))
 
 
 #################
