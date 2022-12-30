0b1484a133c816648997d360226b1e086150793d
hankhank10 <m.a.hankinson@gmail.com>
2022-12-30 09:54:43 +0000

    Adding telegram test

--- telegram_test.py
+++ telegram_test.py
@@ -1,5 +1,8 @@
 import requests
+import logging
 import telegram
+import pprint
+
 
 import secretstuff
 
@@ -9,10 +12,57 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 # Create the app
 app = Flask(__name__)
 
+telegram_api_url = f"https://api.telegram.org/{secretstuff.telegram_full_token}"
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
 	return 'Hello, World Telegram!'
+
+
+@app.route('/telegram/webhook/', methods=['GET', 'POST'])
+def telegram_webhook():
+
+	if request.method == 'POST':
+		# Get the message from the user
+		data = request.get_json()
+		#pprint.pprint(data)
+
+		telegram_message_id = data['update_id']
+		telegram_chat_id = data['message']['chat']['id']
+		mobile = data['message']['from']['id']
+		message_contents = data['message']['text']
+
+		print ("telegram_message_id: " + str(telegram_message_id))
+		print ("telegram_chat_id: " + str(telegram_chat_id))
+		print ("mobile: " + str(mobile))
+		print ("message_contents: " + str(message_contents))
+
+		send_telegram_message(
+			telegram_chat_id,
+			"Hello, World Telegram!"
+		)
+
+	return "ok"
+
+
+def send_telegram_message(
+	telegram_chat_id,
+	message_contents
+):
+	payload = {
+		'chat_id': telegram_chat_id,
+		'text': message_contents,
+	}
+	url = telegram_api_url + '/sendMessage'
+	response = requests.post(url, json=payload)
+
+	print ("Message sent to " + url + " response code was " + str(response.status_code))
+
+	if response.status_code == 200:
+		return True
+	else:
+		return False
 
 
 # Run the app
