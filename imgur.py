a19f8cf83377962dd2d5b2c21f47ebd8bffe192d
hankhank10 <m.a.hankinson@gmail.com>
2022-12-31 10:29:35 +0000

    Update imgur.py

--- imgur.py
+++ imgur.py
@@ -1,15 +1,24 @@
-from os import path
+import os
 from imgur_python import Imgur
 from secretstuff import imgur_client_id, imgur_client_secret
 
 imgur_client = Imgur({'client_id': imgur_client_id})
 
 
-def upload_image(image_path):
+def upload_image(image_path, delete_after_upload=True):
 	image = imgur_client.image_upload(image_path, 'Untitled', 'An upload from LogLink')
 	image_id = image['response']['data']['id']
 
 	if image['response']['data']['link']:
+
+		# Delete the local file
+		if delete_after_upload:
+			if os.path.exists(image_path):
+				os.remove(image_path)
+
 		return image['response']['data']['link']
+
+
+
 	else:
 		return None
