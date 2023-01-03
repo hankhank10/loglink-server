92c00e93069815a9773d0bf22a00297ba700eef0
hankhank10 <m.a.hankinson@gmail.com>
2023-01-03 08:56:22 +0000

    Update secretstuff_template.py

--- secretstuff_template.py
+++ secretstuff_template.py
@@ -1,6 +1,16 @@
 # Whatsapp credentials
 whatsapp_token = ''
 whatsapp_phone_number_id = ""
-verify_token = ''
+whatsapp_verify_token = ''
 
-test_phone_number = ''
+# Database security
+app_secret_key = ''
+
+# Imgur credentials
+imgur_client_id = ''
+imgur_client_secret = ''
+
+# Telegram credentials
+telegram_bot_name = ""
+telegram_token = ""
+telegram_full_token = "bot"+telegram_token
