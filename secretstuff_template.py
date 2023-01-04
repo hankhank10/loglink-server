5e1caad3641c0c689c75ebd9b4cc4de9473f7e34
hankhank10 <m.a.hankinson@gmail.com>
2023-01-04 08:33:34 +0000

    Update secretstuff_template.py

--- secretstuff_template.py
+++ secretstuff_template.py
@@ -1,6 +1,6 @@
 # Whatsapp credentials
 whatsapp_token = ''
-whatsapp_phone_number_id = ""
+whatsapp_phone_number_id = ''
 whatsapp_verify_token = ''
 
 # Database security
@@ -11,7 +11,10 @@ imgur_client_id = ''
 imgur_client_secret = ''
 
 # Telegram credentials
-telegram_bot_name = ""
-telegram_token = ""
-telegram_full_token = "bot"+telegram_token
-telegram_webhook_auth = ""
+telegram_bot_name = ''
+telegram_token = ''
+telegram_full_token = 'bot'+telegram_token
+telegram_webhook_auth = ''
+
+# Sentry
+sentry_dsn = ''
