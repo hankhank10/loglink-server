a88c01b21d2408a6bf9a54df956b4b69b4de2630
hankhank10 <m.a.hankinson@gmail.com>
2023-01-15 12:14:41 +0000

    ✅ Update tests

--- all_test.py
+++ all_test.py
@@ -17,6 +17,41 @@ def test_index():
 		assert b'API is running' not in response.data
 
 
+def test_beta_codes():
+	# Check that beta codes can't be read without the right password
+	with app.test_client() as client:
+		response = client.get(
+			'/admin/beta_codes',
+			headers={"admin-password": "wrong_password"}
+		)
+		assert response.status_code == 401
+		assert b'error' in response.data
+
+	# Check that beta codes can't be added without the right password
+	with app.test_client() as client:
+		response = client.post(
+			'/admin/beta_codes',
+			headers={"admin-password": "wrong_password"},
+			json={"number_of_codes": 1}
+		)
+		assert response.status_code == 401
+		assert b'error' in response.data
+
+	# Check that a beta code can be created when the password is valid
+	with app.test_client() as client:
+		response = client.post(
+			'/admin/beta_codes',
+			headers={"admin-password": envars.admin_password},
+			json={"number_of_codes": 1}
+		)
+		assert response.status_code == 200
+		code_added = response.json["codes_added"][0]
+
+	# Check that the beta code is valid
+	response = project.use_beta_code(code_added)
+	assert response is True
+
+
 def test_get_new_messages():
 	# Check that a nonsense response fails
 	with app.test_client() as client:
@@ -30,24 +65,3 @@ def test_get_new_messages():
 		assert b'error' in response.data
 
 
-def test_security_beta_codes():
-	# Check that beta codes can't be read without the right password
-	with app.test_client() as client:
-		response = client.get(
-			'/admin/beta_codes',
-			headers={"admin-password": "wrong_password"}
-		)
-		assert response.status_code == 401
-		assert b'error' in response.data
-
-	# Check that beta codes can be added without the right password
-	with app.test_client() as client:
-		response = client.post(
-			'/admin/beta_codes',
-			headers={"admin-password": "wrong_password"}
-		)
-		assert response.status_code == 401
-		assert b'error' in response.data
-
-
-# This is a reworked version of this file due to a git problem
