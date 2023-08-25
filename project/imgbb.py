0420f5337b5172910789db9e37ba03a496d3c7ca
hankhank10 <m.a.hankinson@gmail.com>
2023-08-25 12:49:53 +0100

    ➖ Remove unused library

--- project/imgbb.py
+++ project/imgbb.py
@@ -1,54 +1,53 @@
 from . import envars
 import requests
-from pprint import pprint
 import logging
 
 api_url = "https://api.imgbb.com/1/upload"
 
 
 def upload_image(
-	image_path,
-	user_api_token=None,
-	expiration=None,
+        image_path,
+        user_api_token=None,
+        expiration=None,
 ):
 
-	# If no token provided then use the default one
-	if not user_api_token:
-		user_api_token = envars.imgbb_api_key
+    # If no token provided then use the default one
+    if not user_api_token:
+        user_api_token = envars.imgbb_api_key
 
-	# Put together the request
-	payload = {
-		'key': user_api_token,
-	}
-	if expiration:
-		payload['expiration'] = expiration
+    # Put together the request
+    payload = {
+        'key': user_api_token,
+    }
+    if expiration:
+        payload['expiration'] = expiration
 
-	with open(image_path, 'rb') as f:
-		response = requests.post(
-			api_url,
-			files={'image': f},
-			params=payload
-		)
+    with open(image_path, 'rb') as f:
+        response = requests.post(
+            api_url,
+            files={'image': f},
+            params=payload
+        )
 
-	if response.status_code != 200:
-		return False
+    if response.status_code != 200:
+        return False
 
-	image_url = response.json().get('data').get('url')
+    image_url = response.json().get('data').get('url')
 
-	return image_url
+    return image_url
 
 
 def api_key_valid(user_api_token):
-	if not user_api_token:
-		return False
+    if not user_api_token:
+        return False
 
-	# Test an image upload
-	result = upload_image(
-		image_path="test.jpg",
-		user_api_token=user_api_token,
-		expiration=60
-	)
+    # Test an image upload
+    result = upload_image(
+        image_path="test.jpg",
+        user_api_token=user_api_token,
+        expiration=60
+    )
 
-	if not result:
-		return False
-	return True
+    if not result:
+        return False
+    return True
